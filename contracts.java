32354c26166979bfb73a1f5219a69f757e20298d
rhsrc8 <123867552+rhsrc8@users.noreply.github.com>
2026-01-22 16:29:01 +0300

    Add files via upload

---
+++ contracts.java
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleVoting {
+
+    string[] public candidates;
+    mapping(string => uint256) public votes;
+    mapping(address => bool) public hasVoted;
+
+    constructor(string[] memory _candidates) {
+        candidates = _candidates;
+    }
+
+    function vote(string memory _candidate) public {
+        require(!hasVoted[msg.sender], "Zaten oy kullandiniz!");
+
+        bool isValidCandidate = false;
+
+        for (uint i = 0; i < candidates.length; i++) {
+            if (
+                keccak256(bytes(candidates[i])) ==
+                keccak256(bytes(_candidate))
+            ) {
+                isValidCandidate = true;
+                break;
+            }
+        }
+
+        require(isValidCandidate, "Gecersiz aday!");
+
+        votes[_candidate] += 1;
+        hasVoted[msg.sender] = true;
+    }
+
+    function getCandidateCount() public view returns (uint256) {
+        return candidates.length;
+    }
+}
